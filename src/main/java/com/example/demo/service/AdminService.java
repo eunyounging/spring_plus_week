eb88cc92fe4ca4c36ba755a188ce4c611f22c940
“eunyoung” <jyy714@gmail.com>
2024-12-20 22:43:53 +0900

    - TODO: 4. find or save 예제 개선

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,12 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        List<User> users = userRepository.findAllById(userIds);
 
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
+        if (users.isEmpty()) {
+            throw new IllegalArgumentException("해당 아이디에 대한 유저 권한인 사용자가 존재하지 않습니다.");
         }
+
+        userRepository.updatePendingStatus(userIds);
     }
 }
